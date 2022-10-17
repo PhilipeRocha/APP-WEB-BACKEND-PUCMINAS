75ff2a279dc8abaa4ce8eb15285ceb384bafb61d
PhilipeRocha <philiperochad.js@gmail.com>
2022-10-17 20:38:54 -0300

    Add project files.

---
+++ APP-WEB-BACKEND-PUCMINAS/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace APP_WEB_BACKEND_PUCMINAS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
